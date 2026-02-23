0ae319fb592a6a2d5d939c46a53679e71ac9ed50
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2026-02-23 18:44:12 +0530

    Added props for Loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,8 +1,12 @@
-export default function Loader() {
+type LoaderProps = {
+  loaderContent: string;
+};
+
+export default function Loader({ loaderContent }: LoaderProps) {
   return (
     <div className="flex flex-col items-center space-y-4">
       <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-      <p className="text-lg font-medium text-amber-600">Saving to wallet...</p>
+      <p className="text-lg font-medium text-amber-600">{loaderContent}</p>
     </div>
   );
 }
