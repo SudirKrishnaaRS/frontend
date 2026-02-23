1f47e4a02da642d830b454f0823f47a6e5cba4e0
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2026-02-23 18:44:31 +0530

    Added route for Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <main className="flex items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold">Digital Wallet POC</h1>
+
+      <button
+        className="bg-cyan-900 text-white px-4 py-2 rounded"
+        onClick={() => router.push("/wallet")}
+      >
+        Take me to the wallet
+      </button>
     </main>
   );
 }
