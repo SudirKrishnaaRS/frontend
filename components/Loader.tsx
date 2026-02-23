68edd62f04f7f362582d4f5f4c9bf2715a7a491e
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2026-02-23 18:20:16 +0530

    Updated Loader text color

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,7 +2,7 @@ export default function Loader() {
   return (
     <div className="flex flex-col items-center space-y-4">
       <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-      <p className="text-lg font-medium">Saving wallet...</p>
+      <p className="text-lg font-medium text-amber-600">Saving to wallet...</p>
     </div>
   );
 }
